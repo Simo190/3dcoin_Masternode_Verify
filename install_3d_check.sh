92b7925d39913c25232b5413bfa6cb20d4cc08b0
Simone <37718696+Simo190@users.noreply.github.com>
2018-10-11 17:56:53 +0200

    Update install_3d_check.sh

--- install_3d_check.sh
+++ install_3d_check.sh
@@ -9,7 +9,7 @@ NC='\033[0m'
 function prepare_system() {
 
 echo -e "Prepare the system to install ${GREEN} 3dcoin simplify check VPS"
-wget https://raw.githubusercontent.com/Simo190/3dcoin_Masternode_Verify/master/3d.sh && chmod +x 3d_check.sh
+wget https://raw.githubusercontent.com/Simo190/3dcoin_Masternode_Verify/master/3d.sh && chmod +x 3d.sh
 apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" htop unzip >/dev/null 2>&1
 if [ "$?" -gt "0" ];
