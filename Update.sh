b691c0086ef439b3f74087af91b7effd45b17cb0
Simone <37718696+Simo190@users.noreply.github.com>
2019-03-13 17:03:43 +0100

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,5 +1,5 @@
-wget https://github.com/ScaMar/3dcoin-masternode-unofficial/raw/master/3dcoin-linux.zip
-unzip 3dcoin-linux.zip
+wget https://github.com/Simo190/3dcoin_Masternode_Verify/releases/download/V1/bin.zip
+unzip bin.zip
 3dcoin-cli stop
 sleep 20
 killall -15 3dcoind
